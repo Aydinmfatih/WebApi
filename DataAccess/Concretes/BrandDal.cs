55db85141498e8874beb0b30d6e9caa04b7347ba
aydinmfatih <aydinmfatih@gmail.com>
2024-02-25 21:24:34 +0300

    Add project files.

---
+++ DataAccess/Concretes/BrandDal.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes
+{
+    public class BrandDal : IBrandDal
+    {
+        List<Brand> _brands;
+        public BrandDal()
+        {
+            _brands = new List<Brand>();
+            _brands.Add(new Brand
+            {
+                Id = 1,
+                CreatedDate = DateTime.Now,
+                Name = "Bmw"
+            });
+            _brands.Add(new Brand
+            {
+                Id = 2,
+                CreatedDate = DateTime.Now,
+                Name = "Mercedes"
+            });
+            _brands.Add(new Brand
+            {
+                Id = 2,
+                CreatedDate = DateTime.Now,
+                Name = "Alfa Romeo"
+            });
+        }
+        public void Add(Brand brand)
+        {
+            _brands.Add(brand);
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brands;
+        }
+    }
+}
